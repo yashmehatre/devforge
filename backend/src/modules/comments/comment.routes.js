4e5cbda1f49a7551268ded20293594e5efcd7f4f
Yash Mehatre <yashmehatre11@gmail.com>
2026-04-03 23:46:35 +0530

    feat: add comment routes and validation

---
+++ backend/src/modules/comments/comment.routes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const CommentController = require("./comment.controller");
+const { protect } = require("../auth/auth.middleware");
+const validate = require("../../utils/validate");
+const {
+  createCommentSchema,
+  updateCommentSchema,
+} = require("./comment.validation");
+
+const router = express.Router();
+
+router.get("/", CommentController.getAllComments);
+router.post(
+  "/",
+  protect,
+  validate(createCommentSchema),
+  CommentController.createComment,
+);
+router.patch("/:commentId/like", protect, CommentController.toggleLike);
+router.get("/replies/:commentId", CommentController.getAllComments);
+router.get("/:commentId", CommentController.getComment);
+router.patch(
+  "/:commentId",
+  protect,
+  validate(updateCommentSchema),
+  CommentController.updateComment,
+);
+router.delete("/:commentId", protect, CommentController.deleteComment);
+
+module.exports = router;
