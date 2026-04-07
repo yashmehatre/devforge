c03ac0ce88ad8a45f6b42ef55bdd0fcc9fb13e99
Yash Mehatre <yashmehatre11@gmail.com>
2026-04-07 19:04:50 +0530

    fix: add reply creation route and set parentComment on comment creation

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,7 @@ const articleRoutes = require("./modules/articles/article.routes");
 const questionRoutes = require("./modules/questions/question.routes");
 const jobRoutes = require("./modules/jobs/job.routes");
 const applicationRoutes = require("./modules/applications/application.routes");
+const commentRoutes = require("./modules/comments/comment.routes");
 const AppError = require("./utils/AppError");
 
 const app = express();
@@ -55,6 +56,7 @@ app.use("/api/v1/articles", articleRoutes);
 app.use("/api/v1/questions", questionRoutes);
 app.use("/api/v1/jobs", jobRoutes);
 app.use("/api/v1/applications", applicationRoutes);
+app.use("/api/v1/comments", commentRoutes);
 
 // 404 Handler
 
